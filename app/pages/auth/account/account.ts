2afa1bc931328411ff639ca9d4aae81b0990fd34
namnguyen289 <vunam289@gmail.com>
2016-07-06 22:23:44 +0700

    account

--- app/pages/auth/account/account.ts
+++ app/pages/auth/account/account.ts
@@ -1,6 +1,17 @@
-import {Page, NavController, NavParams} from 'ionic-angular';
+import {Page, NavController, NavParams,Platform} from 'ionic-angular';
 import {Storage, LocalStorage, Events} from 'ionic-angular';
 import {UserData} from '../../../services/user-data';
+interface UserInfo {
+    avatarImg: string;
+    userName: string;
+    email: string;
+    phone: string;
+    address: string;
+    bloodType: string;
+    rh: string;
+    gender: string;
+    birthday: any;
+}
 
 @Page({
     templateUrl: 'build/pages/auth/account/account.html'
@@ -10,8 +21,22 @@ export class AccountPage {
     username: string = '';
     authData: any;
     useAvatar: string = '';
+    accountSeg:string = 'infor';
+    isAndroid: boolean = false;
     storage = new Storage(LocalStorage);
-    constructor(public userData: UserData, navParams: NavParams) {
+    userInfo: UserInfo = {
+        avatarImg: '/img/none_avatar.png',
+        userName: '',
+        email: '',
+        phone: '',
+        address: '',
+        bloodType: 'O',
+        rh:'Rh',
+        gender: 'm',
+        birthday: '1988-08-29'
+    };
+
+    constructor(public userData: UserData, navParams: NavParams, platform: Platform) {
         this.authData = navParams.data;
         // console.log(this.authData);
         // console.log(this.useAvatar);
